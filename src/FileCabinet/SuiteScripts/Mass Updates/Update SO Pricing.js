a2242f9f33dac2bb4ea4e311433a7a46fab2c28c
alex.ducken <alexducken@gmail.com>
2022-12-14 15:33:53 -0800

    -Update SO Pricing.js --Initial test on 000-163-034 work continues --Initial commitment and deployment -2023 Customer SO Mass Email.js --Initial commitment and deployment

---
+++ src/FileCabinet/SuiteScripts/Mass Updates/Update SO Pricing.js
@@ -0,0 +1,89 @@
+/**
+ *
+ * @copyright Alex S. Ducken 2022 alexducken@gmail.com
+ * HydraMaster LLC
+ *
+ * @NApiVersion 2.1
+ * @NScriptType MassUpdateScript
+ */
+define(['N/record'],
+    
+    (record) => {
+        /**
+         * Defines the Mass Update trigger point.
+         * @param {Object} params
+         * @param {string} params.type - Record type of the record being processed
+         * @param {number} params.id - ID of the record being processed
+         * @since 2016.1
+         */
+        const each = (params) => {
+            try{
+
+                //Creates object saving line item data for splitting
+                let holdInfo = (record, line, quantity = null, shipped = null) => {
+                    if(quantity != null && shipped != null) {
+                        return {
+                            currentPriceLevel: record.getSublistValue({
+                                fieldId: 'price',
+                                line: line,
+                                sublistId: 'item'
+                            }),
+                            item: record.getLineCount({
+                                fieldId: 'item',
+                                line: line,
+                                sublistId: 'item'
+                            }),
+                            quantity: quantity - shipped
+                        };
+                    }
+                    return null;
+                }
+
+                //Array of New Lines
+                let linesToAdd = [];
+
+                let record = record.load({type: params.type, id: params.id, isDynamic: true});
+
+                for(let x = 0; x < record.getLineCount({sublistId: 'item'}); x++){
+                    let quantity = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'quantity'});
+                    let shipped = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'quantitypickpackship'});
+                    let billed = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'quantitybilled'});
+                    let amount = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'amount'});
+                    let hasPO = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'createpo'});
+                    if(amount > 0 && billed < quantity) {
+                        if (shipped > 0 && hasPO != "DropShip") {
+                            linesToAdd.push(record, x, quantity, shipped);
+                            record.selectLine({sublistId: 'item', line: x});
+                            record.setCurrentSublistValue({sublistId: 'item', fieldId: 'quantity', value: shipped});
+                            record.commitLine({sublistId: 'item'});
+                        }
+                        else{
+                            record.selectLine({sublistId: 'item', line: x});
+                            let costing = record.getCurrentSublistValue({sublistId: 'item', fieldId: 'price'});
+                            record.setCurrentSublistValue({sublistId: 'item', fieldId: 'price', value: 0});
+                            record.setCurrentSublistValue({sublistId: 'item', fieldId: 'price', costing});
+                            record.commitLine({sublistId: 'item'});
+                        }
+                    }
+                }
+
+                if(linesToAdd.length > 0){
+                    for(let x = 0; x < linesToAdd.length; x++) {
+                        record.selectNewLine({sublistId: 'item'});
+                        record.setCurrentSublistValue({sublistId: 'item', fieldId: 'item', value: linesToAdd[x].item});
+                        record.setCurrentSublistValue({sublistId: 'item', fieldId: 'quantity', value: linesToAdd[x].quantity});
+                        record.setCurrentSublistValue({sublistId: 'item', fieldId: 'price', value: linesToAdd[x].currentPriceLevel});
+                        record.commitLine({sublistId: 'item'});
+                    }
+                }
+
+                record.save();
+            }
+            catch (e) {
+                log.error({title: 'Critical error in each', details: e});
+            }
+        }
+
+        return {each}
+
+    });
