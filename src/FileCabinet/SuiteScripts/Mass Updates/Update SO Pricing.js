9ecef20184f27859cf0ee4b987b74266d86f2817
alex.ducken <alexducken@gmail.com>
2022-12-21 15:25:53 -0800

    -Update SO Pricing.js --updating logic, still work to do, testing sourcing of fields.

--- src/FileCabinet/SuiteScripts/Mass Updates/Update SO Pricing.js
+++ src/FileCabinet/SuiteScripts/Mass Updates/Update SO Pricing.js
@@ -6,7 +6,7 @@
  * @NApiVersion 2.1
  * @NScriptType MassUpdateScript
  */
-define(['N/record'],
+define(['N/record', 'N/search'],
     
     (ssrecord) => {
         /**
@@ -44,35 +44,48 @@ define(['N/record'],
 
                 let record = ssrecord.load({type: params.type, id: params.id, isDynamic: false});
 
-                for(let x = 0; x < record.getLineCount({sublistId: 'item'}); x++){
+                for(let x = 0; x < record.getLineCount({sublistId: 'item'}); x++) {
                     let quantity = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'quantity'});
                     let shipped = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'quantitypickpackship'});
                     let billed = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'quantitybilled'});
                     let amount = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'amount'});
                     let hasPO = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'createpo'});
-                    if(amount > 0 && billed < quantity) {
-                        if (shipped > 0 && hasPO != "DropShip") {
+                    let price = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'price'});
+                    let item = record.getSublistValue({sublistId: 'item', line: x, fieldId: 'item'});
+                    let retail = search.lookupFields({type: search.Type.ITEM, id: item, columns: ['baseprice']});
+                    let discount = search.lookupFields({
+                        type: search.Type.PRICE_LEVEL,
+                        id: price,
+                        columns: ['discountpct']
+                    });
+                    //Refactor Testing
+                    log.audit({title: 'Base Price', details: JSON.stringify(retail)});
+                    log.audit({title: 'discount', details: JSON.stringify(discount)});
+                    if (amount > 0 && billed < quantity) {
+                        if (shipped > 0 && hasPO != "DropShip" && price != -1) {
                             linesToAdd.push(holdInfo(record, x, quantity, shipped));
-                            record.setSublistValue({sublistId: 'item', fieldId: 'quantity', value: shipped, line: x});
-                        }
-                        else{
+                            //Refactor Testing
+                            //record.setSublistValue({sublistId: 'item', fieldId: 'quantity', value: shipped, line: x});
+                        } else if (price != -1) {
                             let costing = record.getSublistValue({sublistId: 'item', fieldId: 'price', line: x});
-                            record.setSublistValue({sublistId: 'item', fieldId: 'price', value: 1, line: x});
-                            record.setSublistValue({sublistId: 'item', fieldId: 'price', costing, line: x});}
+                            //Refactor Testing
+                            //record.setSublistValue({sublistId: 'item', fieldId: 'price', value: 1, line: x});
+                            //record.setSublistValue({sublistId: 'item', fieldId: 'price', costing, line: x});}
+                        }
                     }
-                }
 
-                if(linesToAdd.length > 0){
-                    for(let x = 0; x < linesToAdd.length; x++) {
-                        record.selectNewLine({sublistId: 'item'});
-                        record.setCurrentSublistValue({sublistId: 'item', fieldId: 'item', value: linesToAdd[x].item});
-                        record.setCurrentSublistValue({sublistId: 'item', fieldId: 'quantity', value: linesToAdd[x].quantity});
-                        record.setCurrentSublistValue({sublistId: 'item', fieldId: 'price', value: linesToAdd[x].currentPriceLevel});
-                        record.commitLine({sublistId: 'item'});
+                    if (linesToAdd.length > 0) {
+                        for (let x = 0; x < linesToAdd.length; x++) {
+                            let nextLine = record.getLineCount({sublistId: 'item'});
+                            //Refactor Testing
+                            //record.setSublistValue({sublistId: 'item', fieldId: 'item', value: linesToAdd[x].item, line: nextLine});
+                            //record.setSublistValue({sublistId: 'item', fieldId: 'quantity', value: linesToAdd[x].quantity, line: nextLine});
+                            //record.setSublistValue({sublistId: 'item', fieldId: 'price', value: linesToAdd[x].currentPriceLevel, line: nextLine});
+                        }
                     }
-                }
 
-                record.save();
+                    record.save();
+                }
             }
             catch (e) {
                 log.error({title: 'Critical error in each', details: e});
