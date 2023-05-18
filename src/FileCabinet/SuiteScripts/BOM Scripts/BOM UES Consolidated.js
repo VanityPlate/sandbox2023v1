5f36d2d10860336db6f304a89b248166572bebc3
VanityPlate <alexducken@gmail.com>
2023-05-18 13:48:35 -0700

    -Adding Consolidated Scripts for both CS and UES to BOMs --These will add a new column to components and fill it with the item's on hand value

--- src/FileCabinet/SuiteScripts/BOM Scripts/BOM UES Consolidated.js
+++ src/FileCabinet/SuiteScripts/BOM Scripts/BOM UES Consolidated.js
@@ -21,7 +21,7 @@ define(['N/record',
          */
         const beforeLoad = (scriptContext) => {
                 try{
-                     let listToAdd = scriptContext.newRecord.getSubList({sublistId: 'component'});
+                     let listToAdd = scriptContext.form.getSubList({sublistId: 'component'});
                      listToAdd.addField({id: 'quantityOnHand', label: 'On Hand', type: 'INTEGER'});
                 }
                 catch (e) {
