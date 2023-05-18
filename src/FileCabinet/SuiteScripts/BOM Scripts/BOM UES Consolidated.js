d7e16c206f2366704a5f10d6339c4e82653410bc
VanityPlate <alexducken@gmail.com>
2023-05-18 13:35:21 -0700

    -Adding Consolidated Scripts for both CS and UES to BOMs --These will add a new column to components and fill it with the item's on hand value

---
+++ src/FileCabinet/SuiteScripts/BOM Scripts/BOM UES Consolidated.js
@@ -0,0 +1,58 @@
+/**
+ *
+ * @copywrite Alex S. Ducken 2023 alexducken@gmail.com HydraMasterLLC
+ *
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(['N/record',
+                'N/ui/serverWidget'],
+    
+    (record,
+        sWidget) => {
+        /**
+         * Defines the function definition that is executed before record is loaded.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @param {Form} scriptContext.form - Current form
+         * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
+         * @since 2015.2
+         */
+        const beforeLoad = (scriptContext) => {
+                try{
+                     let listToAdd = record.getSubList({sublistId: 'component'});
+                     listToAdd.addField({id: 'quantityOnHand', label: 'On Hand', type: 'INTEGER'});
+                }
+                catch (e) {
+                        log.error(e);
+                }
+        }
+
+        /**
+         * Defines the function definition that is executed before record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const beforeSubmit = (scriptContext) => {
+
+        }
+
+        /**
+         * Defines the function definition that is executed after record is submitted.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
+         * @since 2015.2
+         */
+        const afterSubmit = (scriptContext) => {
+
+        }
+
+        return {beforeLoad}
+
+    });
